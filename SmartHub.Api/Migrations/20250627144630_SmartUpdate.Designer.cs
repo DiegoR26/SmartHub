715d5e5b45c143438e5758a1835d65fef6681f19
Diego Comece <diegoerocha26@gmail.com>
2025-07-04 17:29:34 -0300

    Pequenas alterações na interface

--- SmartHub.Api/Migrations/20250627144630_SmartUpdate.Designer.cs
+++ SmartHub.Api/Migrations/20250627144630_SmartUpdate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SmartHub.Api.Data.Mappings;
 
@@ -10,9 +11,11 @@ using SmartHub.Api.Data.Mappings;
 namespace SmartHub.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250627144630_SmartUpdate")]
+    partial class SmartUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.1");
